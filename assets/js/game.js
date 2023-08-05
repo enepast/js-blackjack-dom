da06ea9a9139b6b646570f852cf5c333e73a9a20
Dario <dnieva@adistec.com>
2023-08-05 00:41:50 -0300

    Added stopBtn functionality

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,12 +4,17 @@ const specials = ['J', 'Q', 'K', 'A'];
 let points = 0;
 let pcPoints = 0;
 
-// Referencias html
+// html references
 const askBtn = document.querySelector('#btnAsk');
+const stopBtn = document.querySelector('#btnStop');
 let scores = document.querySelectorAll('small');
 let playerField = document.querySelector('#player-cards');
+let pcField = document.querySelector('#pc-cards');
 let playerResult = 0;
+let pcResult = 0;
+const perfectScore = 21;
 
+// necessary functions
 const createDeck = () => {
     for (let i = 2; i <= 10; i++) {
         for (const type of types) {
@@ -27,7 +32,6 @@ const createDeck = () => {
 
 const MixedDeck = createDeck();
 
-// necessary functions
 const askCard = (deck) => {
     if (deck.length === 0) throw "There's no cards in the deck";
     const card = deck.pop();
@@ -41,6 +45,7 @@ const valueCard = (card) => {
         value * 1;
 };
 
+// events
 const playCards = (points, field) => {
     const card = askCard(MixedDeck);
     points = points + valueCard(card);
@@ -51,7 +56,6 @@ const playCards = (points, field) => {
     return points;
 }
 
-// events
 askBtn.addEventListener('click', () => {
     playerResult = playCards(points, playerField) + playerResult;
     scores[0].innerText = playerResult;
@@ -64,4 +68,19 @@ askBtn.addEventListener('click', () => {
         askBtn.disabled = true;
         stopBtn.disabled = true;
     }
+});
+
+stopBtn.addEventListener('click', () => {
+    console.log('The PC says: my turn');
+    askBtn.disabled = true;
+    stopBtn.disabled = true;
+    do {
+        pcResult = playCards(pcPoints, pcField) + pcResult;
+    } while (pcResult <= playerResult);
+    scores[1].innerText = pcResult;
+    if (pcResult > perfectScore || pcResult <= playerResult) {
+        scores[1].innerText = pcResult + ' -> says: You win :(';
+    } else if (pcResult > playerResult) {
+        scores[1].innerText = pcResult + ' -> says: I win 3:)';
+    } else scores[1].innerText = pcResult + " -> says: It's a draw :)(";
 });
